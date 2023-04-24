296ee671cb3097d0e1b1a11466d3f702204ab40a
Thorsten Jakoby <thorsten.jakoby@novatec-gmbh.de>
2023-04-24 13:06:37 +0200

    db operator

---
+++ database/installCloudnativepgOperator.sh
@@ -0,0 +1,44 @@
+kubectl apply -f \
+  https://raw.githubusercontent.com/cloudnative-pg/cloudnative-pg/release-1.19/releases/cnpg-1.19.1.yaml
+
+helm repo add prometheus-community \
+  https://prometheus-community.github.io/helm-charts
+
+helm upgrade --install \
+  -f https://raw.githubusercontent.com/cloudnative-pg/cloudnative-pg/main/docs/src/samples/monitoring/kube-stack-config.yaml \
+  prometheus-community \
+  prometheus-community/kube-prometheus-stack
+
+# kubectl apply -f |
+#     apiVersion: v1
+#     kind: Secret
+#     metadata:
+#         name: databasecredentials
+#         namespace: conduit
+#     type: Opaque
+#     data:
+#         username: YWRtaW5Vc2Vy #adminUser
+#         password: cGFzc3dvcmR5 #passwordy
+
+cat <<EOF | kubectl apply -f -
+    apiVersion: postgresql.cnpg.io/v1
+    kind: Cluster
+    metadata:
+        name: conduit-db
+        namespace: conduit
+    spec:
+        instances: 2
+        primaryUpdateStrategy: supervised
+        storage:
+            size: 4Gi
+        monitoring:
+            enablePodMonitor: true
+EOF
+
+kubectl apply -f \
+  https://raw.githubusercontent.com/cloudnative-pg/cloudnative-pg/main/docs/src/samples/monitoring/cnpg-prometheusrule.yaml
+
+kubectl apply -f \
+  https://raw.githubusercontent.com/cloudnative-pg/cloudnative-pg/main/docs/src/samples/monitoring/grafana-configmap.yaml
+
+
