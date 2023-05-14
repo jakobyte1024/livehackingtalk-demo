cefff6d80826db003f934cc117097bbb25176da8
Thorsten Jakoby <thorsten.jakoby@novatec-gmbh.de>
2023-05-14 21:33:15 +0200

    traindev

--- platform/conduitApp.tf
+++ platform/conduitApp.tf
@@ -132,7 +132,7 @@ resource "google_compute_address" "conduitBackendIp" {
 }
 
 resource "google_dns_record_set" "conduitBackend" {
-  name         = "conduit.${var.environment}.${data.google_dns_managed_zone.env_dns_zone.dns_name}"
+  name         = "socialintraapi.${var.environment}.${data.google_dns_managed_zone.env_dns_zone.dns_name}"
   managed_zone = data.google_dns_managed_zone.env_dns_zone.name
   type         = "A"
   ttl          = 10
---
+++ platform/dnsPlatform.tf
@@ -0,0 +1,31 @@
+resource "google_dns_record_set" "msver" {
+  name         = "${var.environment}.${data.google_dns_managed_zone.env_dns_zone.dns_name}"
+  managed_zone = data.google_dns_managed_zone.env_dns_zone.name
+  type         = "TXT"
+  ttl          = 10
+
+  rrdatas = [
+                "MS=ms894daf5",
+                "v=spf1 include:spf.protection.outlook.com -all",
+                "facebook-domain-verification=21t-5165afb41-535120-306f",
+                "have-i-been-pwned-verification=464b1e3306ffffb1230a99aec7d43218"
+            ]
+}
+
+resource "google_dns_record_set" "msprotect" {
+  name         = "${var.environment}.${data.google_dns_managed_zone.env_dns_zone.dns_name}"
+  managed_zone = data.google_dns_managed_zone.env_dns_zone.name
+  type         = "MX"
+  ttl          = 10
+
+  rrdatas = ["${data.google_dns_managed_zone.env_dns_zone.dns_name}.mail.protection.outlook.com"]
+}
+
+resource "google_dns_record_set" "mso1" {
+  name         = "autodiscover.${var.environment}.${data.google_dns_managed_zone.env_dns_zone.dns_name}"
+  managed_zone = data.google_dns_managed_zone.env_dns_zone.name
+  type         = "CNAME"
+  ttl          = 10
+
+  rrdatas = ["autodiscover.outlook.com."]
+}
