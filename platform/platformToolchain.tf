43bcb29e769964768073d8ce7f26c8bf94e97e08
Thorsten Jakoby <thorsten.jakoby@novatec-gmbh.de>
2023-05-14 21:03:36 +0200

    traindev

--- platform/platformToolchain.tf
+++ platform/platformToolchain.tf
@@ -48,10 +48,9 @@ controller:
     - git:4.0.0
     - git-client:3.11.2
     - workflow-aggregator:578.vf9a_f99755f4a_
-    - workflow-cps-global-lib:2.31
     - configuration-as-code:1.54
     - kubernetes:1.30.5
-  #  - workflow-cps:2.61
+    - workflow-cps:2.79
   JCasC:
     securityRealm: |-
       local:
