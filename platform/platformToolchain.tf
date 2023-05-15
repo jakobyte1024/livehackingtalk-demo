1b7bc0bbf2f126c36614a6d4a0138fa1f07802d5
Thorsten Jakoby <thorsten.jakoby@novatec-gmbh.de>
2023-05-15 14:32:21 +0200

    traindev

--- platform/platformToolchain.tf
+++ platform/platformToolchain.tf
@@ -110,19 +110,18 @@ controller:
                 maven('-e clean test')
             }
         }
-  - script: |-
-      job('testJob2') {
-          scm {
-              git('git://github.com/quidryan/aws-sdk-test.git')
+      - script: |-
+          job('testJob2') {
+              scm {
+                  git('git://github.com/quidryan/aws-sdk-test.git')
+              }
+              triggers {
+                  scm('H/15 * * * *')
+              }
+              steps {
+                  maven('-e clean test')
+              }
           }
-          triggers {
-              scm('H/15 * * * *')
-          }
-          steps {
-              maven('-e clean test')
-          }
-      }
-
 EOF
   ]
 }
