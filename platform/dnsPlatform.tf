7ecf30eecd58b6e48e6aa28d0f46568c08d6e030
Thorsten Jakoby <thorsten.jakoby@novatec-gmbh.de>
2023-05-15 13:25:14 +0200

    traindev

--- platform/dnsPlatform.tf
+++ platform/dnsPlatform.tf
@@ -21,7 +21,7 @@ resource "google_dns_record_set" "mso1" {
   rrdatas = ["autodiscover.outlook.com."]
 }
 
-resource "google_dns_record_set" "stageCnApp" {
+resource "google_dns_record_set" "stageCn" {
   name         = "${var.environment}.${data.google_dns_managed_zone.env_dns_zone.dns_name}"
   managed_zone = data.google_dns_managed_zone.env_dns_zone.name
   type         = "CNAME"
