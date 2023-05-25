6e0b0258fc8c0cee505fd8fd93f2d67efb7f69f4
MirnaAlaisami <mirna.alaisami@gmail.com>
2023-05-25 10:36:18 +0200

    set static IP for ingress controller

--- platform/step2/platformInfrastructure.tf
+++ platform/step2/platformInfrastructure.tf
@@ -9,3 +9,11 @@ data "google_service_account" "conduit" {
 data "google_container_cluster" "primary" {
   name        = "conduit-k8s-${var.environment}"
 }
+
+resource "google_compute_address" "ingressIp" {
+  name          = "ingressip${var.environment}"
+  region        = "europe-west3"
+  address_type  = "EXTERNAL"
+}
+
+
