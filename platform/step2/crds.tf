c5c5acd316424901cb30f5575872cd4e3418df01
MirnaAlaisami <mirna.alaisami@gmail.com>
2023-05-25 08:38:35 +0200

    configure ingress rules

--- platform/step2/crds.tf
+++ platform/step2/crds.tf
@@ -38,6 +38,6 @@ resource "kubernetes_namespace" "ingressNginx" {
 #}
 
 resource "kubernetes_manifest" "ingressNginxController" {
-  manifest = "https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.5.1/deploy/static/provider/cloud/deploy.yaml"
+  manifest = echo 'yamldecode(file("https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.5.1/deploy/static/provider/cloud/deploy.yaml"))'
 
 }
