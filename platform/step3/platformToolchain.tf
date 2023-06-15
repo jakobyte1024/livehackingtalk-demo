f440bd8efd81ebea1dd58fb6d3448888692d6c6f
MirnaAlaisami <mirna.alaisami@gmail.com>
2023-06-15 15:04:06 +0200

    downgrade jenkins helm release

--- platform/step3/platformToolchain.tf
+++ platform/step3/platformToolchain.tf
@@ -344,7 +344,6 @@ resource "kubernetes_manifest" "jenkinsIngress" {
       "annotations" = {
         "kubernetes.io/ingress.class" = "nginx"
         "nginx.ingress.kubernetes.io/rewrite-target" = "/"
-        "nginx.ingress.kubernetes.io/ssl-redirect" = "false"
       }
     }
 
