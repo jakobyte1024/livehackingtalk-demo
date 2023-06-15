2b30b20ff9bd4c07964e6b0cc2e0e6762650620d
MirnaAlaisami <mirna.alaisami@gmail.com>
2023-06-15 11:21:46 +0200

    fix jenkins helm release plugins options

--- platform/step3/platformToolchain.tf
+++ platform/step3/platformToolchain.tf
@@ -63,8 +63,8 @@ rbac:
 controller:
   tag: 2.387.3-lts-jdk11
   serviceType: ClusterIP
-  #installLatestPlugins: true
-  #overwritePluginsFromImage: false
+  installLatestPlugins: false
+  overwritePluginsFromImage: true
   installPlugins:
     - git:5.0.0
     - configuration-as-code:1625.v27444588cc3d
