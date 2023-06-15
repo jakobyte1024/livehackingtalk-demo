aa3643887343f54e387189a6f4f00f4c02ea9728
MirnaAlaisami <mirna.alaisami@gmail.com>
2023-06-15 14:59:37 +0200

    downgrade jenkins helm release

--- platform/step2/crds.tf
+++ platform/step2/crds.tf
@@ -26,7 +26,7 @@ resource "helm_release" "ingressNginxController" {
   repository = "https://kubernetes.github.io/ingress-nginx/"
   chart      = "ingress-nginx"
   namespace  = "ingress-nginx"
-  version = "4.2.5"
+  version = "4.3.0"
 
   set {
     name  = "ingressClassResource.default"
