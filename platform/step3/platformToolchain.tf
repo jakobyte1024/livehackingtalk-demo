fcd64d678b4f6eedc79ac1700ab687dbcdde169a
MirnaAlaisami <mirna.alaisami@gmail.com>
2023-06-16 10:28:16 +0200

    edit ingress rules

--- platform/step3/platformToolchain.tf
+++ platform/step3/platformToolchain.tf
@@ -343,7 +343,8 @@ resource "kubernetes_manifest" "jenkinsIngress" {
       "namespace" = "toolchain"
       "annotations" = {
         "kubernetes.io/ingress.class" = "nginx"
-        "nginx.ingress.kubernetes.io/rewrite-target" = "/*"
+        "nginx.ingress.kubernetes.io/rewrite-target" = "/"
+        "nginx.ingress.kubernetes.io/ssl-redirect" = "false"
       }
     }
 
