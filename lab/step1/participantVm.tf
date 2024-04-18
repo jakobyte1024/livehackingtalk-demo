3189c325f014d341f39fc4db92645b75f3531604
Thorsten Jakoby <thorsten.jakoby@novatec-gmbh.de>
2024-04-18 12:53:02 +0200

    vpc

--- lab/step1/participantVm.tf
+++ lab/step1/participantVm.tf
@@ -1,4 +1,19 @@
-data "google_compute_image" "my_image" {
-  name          = "lhca-attackvm-base-1"
-  project       = "thorsten-jakoby-tj-projekt"
+resource "google_compute_network" "vpc_network" {
+  name = "${var.labName}-vpc"
+}
+
+resource "google_compute_instance_from_machine_image" "participantVm" {
+  provider = google-beta
+  name     = "${var.labName}-participant-0"
+  zone     = "europe-west3-c"
+
+  source_machine_image = "projects/thorsten-jakoby-tj-projekt/global/machineImages/lhca-attackvm-base-1"
+
+  // Override fields from machine image
+  network_interface {
+    network = "${var.labName}-vpc"
+    network_ip = ""
+
+  }
+
 }
